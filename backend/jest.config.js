685acaa4df0cc2898ac0b9345aa4c5bd357b476a
Darren Holm <darren@holmgraphics.ca>
2026-05-19 08:54:52 -0400

    Fix tsconfig rootDir for tests, add vite env types, drop jest config typo

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -3,9 +3,11 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>/src', '<rootDir>/tests'],
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.test.json' }],
+  },
   testMatch: ['**/*.test.ts'],
   moduleFileExtensions: ['ts', 'js', 'json'],
-  setupFilesAfterEach: [],
   clearMocks: true,
   collectCoverageFrom: ['src/**/*.ts', '!src/**/*.d.ts', '!src/server.ts'],
 };
